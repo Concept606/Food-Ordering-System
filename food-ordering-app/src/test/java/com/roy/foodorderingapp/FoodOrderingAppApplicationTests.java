0a695ec1c8198b03367e9eebd846eadaead47561
Concept606 <amitbharoy98@gmail.com>
2024-02-26 22:44:22 +0530

    spring build complete

---
+++ food-ordering-app/src/test/java/com/roy/foodorderingapp/FoodOrderingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roy.foodorderingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
